2ad30247c5a1a52e5c29774d636b2c59eba292f7
DivyaHGouda <157840679+DivyaHGouda@users.noreply.github.com>
2024-05-15 10:03:12 +0530

    Time: 414 ms (95.19%), Space: 27.5 MB (46.91%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hashset=set()
+        for i in nums:
+            if i in hashset:
+                return True
+            else:
+                hashset.add(i)
+        return False
+        
