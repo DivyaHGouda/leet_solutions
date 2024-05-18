533e7972ca99ae23e3e3ab3a3f5c63917bbcf2dc
DivyaHGouda <157840679+DivyaHGouda@users.noreply.github.com>
2024-05-18 16:38:31 +0530

    Time: 7 ms (32.76%), Space: 40.6 MB (88.05%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,13 +1,11 @@
 class Solution {
     public int arrangeCoins(int n) {
         int j=0,k=n;
-        for(int i=1;i<=k;i++){
-            if(k-i>=i || k-i==0){
-                k=k-i;
-                j++;
-                if(k==0)
-                break;
-            }
+        int i=1;
+        while(k>=i){
+            k=k-i;
+            j++;
+            i++;
         }
         return j;
     }
